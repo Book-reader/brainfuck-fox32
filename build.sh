1b0cacaf6d79df8255e6dcc7f705634bfa065bd1
Book-reader <thevoid@outlook.co.nz>
2025-04-08 19:58:16 +1200

    restructuring

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-./bf2fox32 "${1}.b" "${1}_b.asm"
+./bf2fox32 "files/${1}.b" "build/${1}_b.asm"
 
-fox32asm "${1}_b.asm" "${1}.fxf"
+fox32asm "build/${1}_b.asm" "out/${1}.fxf"
 
-ryfs.py add brainfrick.img "${1}.fxf"
+ryfs.py add brainfrick.img "out/${1}.fxf"
