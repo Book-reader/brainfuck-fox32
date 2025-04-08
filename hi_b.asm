c081b815c197de7c5378ab4443ab58ada402c7f1
Book-reader <thevoid@outlook.co.nz>
2025-04-08 16:08:38 +1200

    move codegen mostly to a struct with methods that wrap fprintf

--- hi_b.asm
+++ hi_b.asm
@@ -1,688 +1,330 @@
-
-pop [stream_ptr]
-pop [arg_0]
-pop [arg_1]
-pop [arg_2]
-pop [arg_3]
-
-mov r3, 0
-mov r4, 0
-
-call yield_task
-
-mov r5, 0
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 1
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 2
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+pop.32 [stream_ptr]
+pop.32 [arg_0]
+pop.32 [arg_1]
+pop.32 [arg_2]
+pop.32 [arg_3]
+mov.32 r3, 0
+mov.32 temp_ch, 0
+mov.32 r5, 1
+add.32 r3, 1
+mov.32 r5, 2
+add.32 r3, 1
+mov.32 r5, 3
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 3
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 4
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 4
+sub.32 r3, 1
+mov.32 r5, 5
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 5
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 6
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 6
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 7
+mov.32 r4, mem
+add.32 r4, r3
 cmp.8 0, [r4]
-ifz jmp loop_end_0
+ifzjmp loop_end_0
 loop_start_0:
-
-
-mov r5, 7
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 8
+mov.32 r4, mem
+add.32 r4, r3
 cmp.8 0, [r4]
-ifz jmp loop_end_1
+ifzjmp loop_end_1
 loop_start_1:
-
-
-mov r5, 8
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 9
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 9
+sub.32 r3, 1
+mov.32 r5, 10
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 10
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 11
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 11
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 12
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 12
+add.32 r3, 1
+mov.32 r5, 13
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 13
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 14
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 14
+add.32 r3, 1
+mov.32 r5, 15
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 15
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 16
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 16
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 17
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 17
+add.32 r3, 1
+mov.32 r5, 18
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 18
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 19
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 19
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 20
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 20
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 21
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 21
+add.32 r3, 1
+mov.32 r5, 22
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 22
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 23
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 24
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 25
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 23
+sub.32 r3, 1
+mov.32 r5, 24
+sub.32 r3, 1
+mov.32 r5, 25
+sub.32 r3, 1
+mov.32 r5, 26
+mov.32 r4, mem
+add.32 r4, r3
 cmp.8 0, [r4]
-ifnz jmp loop_start_1
+ifnzjmp loop_start_1
 loop_end_1:
-
-
-mov r5, 26
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 27
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 27
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 28
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 28
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 29
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 29
+add.32 r3, 1
+mov.32 r5, 30
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 30
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 31
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 31
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 32
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 32
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 33
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 33
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 34
+mov.32 r4, mem
+add.32 r4, r3
 cmp.8 0, [r4]
-ifnz jmp loop_start_0
+ifnzjmp loop_start_0
 loop_end_0:
-
-
-mov r5, 34
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 35
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 36
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 35
+sub.32 r3, 1
+mov.32 r5, 36
+sub.32 r3, 1
+mov.32 r5, 37
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 37
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 38
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 39
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 38
+sub.32 r3, 1
+mov.32 r5, 39
+sub.32 r3, 1
+mov.32 r5, 40
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 40
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 41
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 41
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 42
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 43
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 42
+sub.32 r3, 1
+mov.32 r5, 43
+sub.32 r3, 1
+mov.32 r5, 44
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 44
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 45
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 45
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 46
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 46
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 47
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 47
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 48
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 48
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 49
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 49
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
 call yield_task
-
-mov r5, 50
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 50
+add.32 r3, 1
+mov.32 r5, 51
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 51
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 52
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 53
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 52
+sub.32 r3, 1
+mov.32 r5, 53
+sub.32 r3, 1
+mov.32 r5, 54
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 54
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 55
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 55
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 56
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 56
+add.32 r3, 1
+mov.32 r5, 57
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 57
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 58
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 58
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 59
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 59
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 60
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 60
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 61
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 61
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 62
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 62
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 63
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 63
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 64
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 64
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 65
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 65
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 66
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 66
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 67
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 67
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 68
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 68
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 69
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 70
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 69
+add.32 r3, 1
+mov.32 r5, 70
+add.32 r3, 1
+mov.32 r5, 71
+mov.32 r4, mem
+add.32 r4, r3
 sub.8 [r4], 1
-
-
-mov r5, 71
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 72
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 72
-cmp r3, 1000
-ifgt brk
-
-sub r3, 1
-
-mov r5, 73
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
+mov.32 r5, 73
+sub.32 r3, 1
+mov.32 r5, 74
+mov.32 r4, mem
+add.32 r4, r3
 add.8 [r4], 1
-
-
-mov r5, 74
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 75
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 75
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 76
-cmp r3, 1000
-ifgt brk
-
-add r3, 1
-
-mov r5, 77
-cmp r3, 1000
-ifgt brk
-
-mov r4, mem
-add r4, r3
-mov.8 [temp], [r4]
-mov r2, temp
-mov r0, 1
-mov r1, [stream_ptr]
+mov.32 r5, 76
+add.32 r3, 1
+mov.32 r5, 77
+add.32 r3, 1
+mov.32 r5, 78
+mov.32 r4, mem
+add.32 r4, r3
+mov.8 [temp_ch], r4
+mov.32 r2, temp_ch
+mov.32 r0, 1
+mov.32 r1, [stream_ptr]
 call write
-
-mov r5, 78
-cmp r3, 1000
-ifgt brk
-
+mov.32 r5, 79
 call end_current_task
 
 stream_ptr: data.32 0
@@ -690,7 +332,7 @@ arg_0: data.32 0
 arg_1: data.32 0
 arg_2: data.32 0
 arg_3: data.32 0
-temp: data.8 0
+temp_ch: data.8 0
 mem: data.fill 0, 30000
 #include "./fox32rom/fox32rom.def"
 #include "./fox32os/fox32os.def"
